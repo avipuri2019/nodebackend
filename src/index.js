0ffb8856b9f17310033018070c1769adeb7b9ba1
avipuri2019 <avinashpuri2016@gmail.com>
2023-11-18 00:37:54 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import connectDB from "./db/db.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed !!", err);
+  });
 // import { express } from "express";
 // const app = express();
 
