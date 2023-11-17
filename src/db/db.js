837ee3f877546b76f698f92e9adb9c7d0b70f8d5
avipuri2019 <avinashpuri2016@gmail.com>
2023-11-17 09:24:05 +0530

    db connection setup

---
+++ src/db/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
+      { useNewUrlParser: true }
+    );
+    console.log(`\n MongoDB connected !! DB Host:${connectionInstance}`);
+  } catch (error) {
+    console.log("MongoDB connection error: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
