6b4a1df6c630acc2b97ce1bfefe66ec0f43c9bc9
avipuri2019 <avinashpuri2016@gmail.com>
2023-11-17 09:24:54 +0530

    db connection issue fixed

--- src/db/db.js
+++ src/db/db.js
@@ -4,8 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`,
-      { useNewUrlParser: true }
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(`\n MongoDB connected !! DB Host:${connectionInstance}`);
   } catch (error) {
